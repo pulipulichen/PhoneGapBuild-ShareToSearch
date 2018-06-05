bcb2de8245ab948cac94fa4689bb09c0855060d6
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-06-05 22:39:54 +0800

    0605 輔助文字也可以了

--- test/test_script.js
+++ test/test_script.js
@@ -1,6 +1,6 @@
 intent_handler = function (intent) {
-    alert("換了 可以嗎？");
-    alert(JSON.stringify(intent));
+    //alert("換了 可以嗎？");
+    //alert(JSON.stringify(intent));
     if (typeof (intent.action) === "string"
             && intent.action === "android.intent.action.MAIN") {
         // 沒有要檢索的東西，回家吧。
@@ -17,18 +17,35 @@ intent_handler = function (intent) {
 
     if (typeof (intent.extras) === "object") {
         var _extras = intent.extras;
-        if (_has_string(_extras["android.intent.extra.SUBJECT"])) {
-            _search_items.push(_extras["android.intent.extra.SUBJECT"].trim());
+
+        var _key_list = [
+            "android.intent.extra.SUBJECT",
+            "android.intent.extra.TEXT",
+            "android.intent.extra.PROCESS_TEXT",
+        ];
+
+        for (var _i = 0; _i < _key_list.length; _i++) {
+            if (_has_string(_extras[_key_list[_i]])) {
+                _search_items.push(_extras[_key_list[_i]].trim());
+            }
         }
-        if (_has_string(_extras["android.intent.extra.TEXT"])) {
-            _search_items.push(_extras["android.intent.extra.TEXT"].trim());
-        }
+        /*
+         if (_has_string(_extras["android.intent.extra.SUBJECT"])) {
+         _search_items.push(_extras["android.intent.extra.SUBJECT"].trim());
+         }
+         if (_has_string(_extras["android.intent.extra.TEXT"])) {
+         _search_items.push(_extras["android.intent.extra.TEXT"].trim());
+         }
+         if (_has_string(_extras["android.intent.extra.PROCESS_TEXT"])) {
+         _search_items.push(_extras["android.intent.extra.PROCESS_TEXT"].trim());
+         }
+         */
     }
 
     var _test_url = _search_items.join(" ");
     _test_url = _test_url.split("\n").join(" ");
     var _url_list = [];
-    
+
     var _http_list = _test_url.split("http://");
     for (var _i = 1; _i < _http_list.length; _i++) {
         var item = _http_list[_i];
@@ -59,7 +76,7 @@ intent_handler = function (intent) {
     if (_url_list.length > 0) {
         for (var i = 0; i < _url_list.length; i++) {
             //setTimeout(function () {
-                window.open(_url_list[i], "_system");
+            window.open(_url_list[i], "_system");
             //}, 0);
         }
         navigator.app.exitApp();
@@ -72,14 +89,53 @@ intent_handler = function (intent) {
                 && (_search_items[0].startsWith("http://") || _search_items[0].startsWith("https://"))) {
             //alert(encodeURIComponent(_search_items[0]));
             window.open(_search_items[0], "_system");
+            navigator.app.exitApp();
         } else {
-            var _url = "https://www.google.com/search?q=" + encodeURIComponent(_search_items.join(" "));
-            window.open(_url, "_system");
+            //var _url = "https://www.google.com/search?q=" + encodeURIComponent(_search_items.join(" "));
+            //var _url = "android-app://com.google.android.googlequicksearchbox/" + encodeURIComponent(_search_items.join(" "));
+
+            //window.open(_url, "_system");
+
+            var _search_text = _search_items.join(" ");
+
+            var _config = {
+                //action: "android.app.SearchManager.INTENT_ACTION_GLOBAL_SEARCH",
+                action: "android.intent.action.WEB_SEARCH",
+                //data: _search_text,
+                //uri: _search_text,
+                //url: _search_text,
+                //pacakge: "com.google.android.googlequicksearchbox",
+                extras: {
+                    //"android.intent.extra.SUBJECT": _search_text,
+                    //"android.intent.extra.TEXT": _search_text,
+                    //"ACTION_MSG": 1,
+                    //"ACTION_MSG": 1,
+                    "query": _search_text,
+                    //"SearchManager": {
+                    //    "QUERY": _search_text,
+                    //}
+                }
+            };
+
+            try {
+                window.plugins.webintent.startActivity(_config,
+                        function () {
+                            navigator.app.exitApp();
+                        },
+                        function () {
+                            alert('Failed:' + JSON.stringify(_search_items.join(" "), null, 2));
+                            navigator.app.exitApp();
+                        }
+                );
+            } catch (e) {
+                alert(e);
+            }
         }
     }
     //alert([JSON.stringify(_search_items)
     //    , _search_items.length === 1
     //    , _search_items[0].startsWith("http://") 
     //    , _search_items[0].startsWith("https://")]);
-    navigator.app.exitApp();
+
+    //navigator.app.exitApp();
 };
