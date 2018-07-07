878c57ed2b76b0cf1bdabd68d57e1fc08effa6be
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-07-07 13:40:17 +0800

    0707 修復

--- test/test_script.js
+++ test/test_script.js
@@ -1,6 +1,6 @@
 intent_handler = function (intent) {
     //alert("換了 可以嗎？");
-    alert(JSON.stringify(intent));
+    //alert(JSON.stringify(intent));
     
     if (typeof (intent.action) === "string"
             && intent.action === "android.intent.action.MAIN") {
@@ -26,12 +26,14 @@ intent_handler = function (intent) {
             "android.intent.extra.TEXT",
             "android.intent.extra.PROCESS_TEXT",
         ];
+        
+        //alert(_key_list.length);
 
         for (var _i = 0; _i < _key_list.length; _i++) {
             if (_has_string(_extras[_key_list[_i]])) {
                 var _subject = _extras[_key_list[_i]].trim();
-                for (var _i = 0; _i < FILTER_SUBJECT.length; _i++) {
-                    var _needle = FILTER_SUBJECT[_i];
+                for (var _j = 0; _j < FILTER_SUBJECT.length; _j++) {
+                    var _needle = FILTER_SUBJECT[_j];
                     if (_subject === _needle) {
                         //_text = _text.substring(_needle.length, _text.length).trim();
                         _subject = null;
@@ -54,6 +56,8 @@ intent_handler = function (intent) {
          _search_items.push(_extras["android.intent.extra.PROCESS_TEXT"].trim());
          }
          */
+        
+        //alert(JSON.stringify(_search_items));
     }
 
     var _test_url = _search_items.join(" ");
@@ -85,6 +89,8 @@ intent_handler = function (intent) {
         }
         _url_list.push("https://" + item.substring(0, pos));
     }
+    
+    //alert(_url_list.length);
 
     //alert(JSON.stringify(_url_list));
     if (_url_list.length > 0) {
